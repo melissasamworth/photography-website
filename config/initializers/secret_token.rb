fce8108cdfd4dbcd0d81b5908db10328b5f06b98
Melissa Samworth <melissasamworth@macbook.home>
2011-05-31 21:58:07 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotographyWebsite::Application.config.secret_token = '0ba1457dfe7f4cc11db0667c28fcbb33743bc1b35a277c6c8f2e5c5604f8c7c2c4985dded87ef4b59a48f7bf975bb7fb55d1c69498ead23d78d148eee34e5636'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PhotographyWebsite::Application.config.session_store :cookie_store, :key => '_photography_website_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PhotographyWebsite::Application.config.session_store :active_record_store
